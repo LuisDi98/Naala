91205dc6a7f4c536b36a0225e584b0b4f699887b
villarley <santivillarley1010@gmail.com>
2025-01-17 10:09:46 -0600

    feat: main text added

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,5 +1,10 @@
 export default function Main() {
   return (
-    <div>Main</div>
-  )
+    <div className="flex flex-col justify-center items-center h-screen">
+      <p className="text-5xl">
+        Seleccioná el hogar
+        <b> que siempre soñaste</b>
+      </p>
+    </div>
+  );
 }
