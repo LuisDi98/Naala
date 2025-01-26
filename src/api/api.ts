b3d6f5be7b6deb0919338ca92b2a9ee8f1e3eea0
LuisDi98 <aguilarluisdi@gmail.com>
2025-01-26 15:20:52 -0600

    feat: update API base URL to production endpoint

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
+
   baseURL: "https://naala-api.vercel.app/api/",
-  //baseURL: "http://localhost:5500/api/",
   headers: {
     "Content-Type": "application/json",
   },
